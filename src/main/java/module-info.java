35b914940044ac5422f6cba99be333fd7bba2eea
ss kim <122724312+hyyy3@users.noreply.github.com>
2024-11-01 18:19:16 +0900

    Initial commit - Maven

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module core.npteamproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens core.npteamproject to javafx.fxml;
+    exports core.npteamproject;
+}
