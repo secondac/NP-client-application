e769d5e148558820712b3dd8b2837b23b44e3cf9
ss kim <122724312+hyyy3@users.noreply.github.com>
2024-11-02 21:39:49 +0900

    2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,11 @@ module com.examples {
     requires com.almasb.fxgl.all;
 
     opens core.$legacyfiles to javafx.fxml;
+    opens core.view to javafx.fxml;
+    opens core.controller to javafx.fxml;
+    opens core.service to javafx.fxml;
+    opens core.model to javafx.fxml;
+
     exports core.$legacyfiles;
     exports core.controller;
     exports core.model;
