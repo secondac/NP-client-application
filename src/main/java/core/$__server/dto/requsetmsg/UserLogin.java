d127fb9969482924cf1b359c362010465b5be92a
ss kim <122724312+hyyy3@users.noreply.github.com>
2024-11-24 23:57:37 +0900

    로그인 통신 시도1

---
+++ src/main/java/core/$__server/dto/requsetmsg/UserLogin.java
@@ -0,0 +1,14 @@
+package core.$__server.dto.requsetmsg;
+
+public class UserLogin {
+    private String username;
+
+    public UserLogin(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
+
---
+++ src/main/java/core/$__server/dto/response/ChatRoom.java
@@ -0,0 +1,63 @@
+package core.$__server.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class ChatRoom {
+    Integer id;
+    String name;
+    String creator;
+    LocalDateTime time;
+
+    public ChatRoom() {
+    }
+
+    public ChatRoom(Integer id, String name, String creator, LocalDateTime time) {
+        this.id = id;
+        this.name = name;
+        this.creator = creator;
+        this.time = time;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChatRoom chatRoom = (ChatRoom) o;
+        return Objects.equals(id, chatRoom.id) &&
+                Objects.equals(name, chatRoom.name) &&
+                Objects.equals(creator, chatRoom.creator) &&
+                Objects.equals(time, chatRoom.time);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, creator, time);
+    }
+
+    @Override
+    public String toString() {
+        return "ChatRoom{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", creator='" + creator + '\'' +
+                ", time=" + time +
+                '}';
+    }
+}
