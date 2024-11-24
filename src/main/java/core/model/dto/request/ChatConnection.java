b8be8269b54adb7129821aefcf4286be7fc6edcd
ss kim <122724312+hyyy3@users.noreply.github.com>
2024-11-25 02:25:36 +0900

    DTO 추가

---
+++ src/main/java/core/model/dto/request/ChatConnection.java
@@ -0,0 +1,22 @@
+package core.model.dto.request;
+
+public class ChatConnection {
+    private Integer userId;
+    private Integer chatId;
+
+    public ChatConnection() {
+    }
+
+    public ChatConnection(Integer userId, Integer chatId) {
+        this.userId = userId;
+        this.chatId = chatId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public Integer getChatId() {
+        return chatId;
+    }
+}
---
+++ src/main/java/core/model/dto/response/ListChatRoom.java
@@ -0,0 +1,25 @@
+package core.model.dto.response;
+
+import java.util.List;
+
+public class ListChatRoom {
+    List<ChatRoom> chatRooms;
+
+    public ListChatRoom() {
+    }
+
+    public ListChatRoom(List<ChatRoom> chatRooms) {
+        this.chatRooms = chatRooms;
+    }
+
+    public List<ChatRoom> getChatRooms() {
+        return chatRooms;
+    }
+
+    @Override
+    public String toString() {
+        return "ListChatRoom{" +
+                "chatRooms=" + chatRooms +
+                '}';
+    }
+}
