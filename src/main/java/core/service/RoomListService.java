11ab73c802e4296f1956529e3be071eeccbcd794
ss kim <122724312+hyyy3@users.noreply.github.com>
2024-11-28 10:17:12 +0900

    RoomListService 작성중

--- src/main/java/core/service/RoomListService.java
+++ src/main/java/core/service/RoomListService.java
@@ -1,4 +1,56 @@
+/**
+ * RoomListService : 클라이언트가 로그인 후, 서버로 요청을 보냄
+ * @Param Thread 사용
+ * */
+
 package core.service;
 
-public class RoomListService {
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import com.google.gson.Gson;
+
+import core.model.Room;
+import core.model.dto.request.*;
+import core.model.dto.response.*;
+import core.model.dto.*;
+
+
+public class RoomListService extends Thread {
+
+    private static final int SERVER_PORT = 10001;
+    private final Gson gson = new Gson();
+
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    // private String roomName;
+    // ArrayList로 구현 ?
+    /**
+     * rooms : 방 목록을 ArrayList generic으로 담음
+     */
+
+    private ArrayList<Room> rooms;
+
+
+
+    @Override
+    public void run() {
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
 }
